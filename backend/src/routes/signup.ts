483baed6e08cba682eb5dd4b1d3f346c267b57ec
alfaArghya <arghya.coding@gmail.com>
2024-04-01 03:06:38 +0530

    signup route

--- backend/src/routes/signup.ts
+++ backend/src/routes/signup.ts
@@ -1,9 +1,46 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
+import * as argon2 from "argon2";
+import { status } from "../responseStatus/responseStatus";
 
-const signup = (req: Request, res: Response) => {
-  res.json({
-    message: "Hello user! First sign up",
-  });
+const prisma = new PrismaClient();
+dotenv.config();
+
+const signup = async (req: Request, res: Response) => {
+  const name: string = req.body.name;
+  const email: string = req.body.email;
+  const username: string = req.body.username;
+  const password: string = req.body.password;
+
+  const hashPassword = await argon2.hash(password);
+
+  try {
+    const createUser = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        username: username,
+        password: hashPassword,
+      },
+    });
+
+    const token = jwt.sign(
+      { id: createUser.id, username: createUser.username },
+      `${process.env.JWT_Secret}`
+    );
+
+    res.status(status.Success).json({
+      message: "signup successful",
+      token,
+    });
+  } catch (err) {
+    res
+      .status(status.Conflict)
+      .json({ msg: "username or email already exist" });
+    return;
+  }
 };
 
 export default signup;
