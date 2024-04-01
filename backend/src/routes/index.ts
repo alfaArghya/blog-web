224078ef4f7cb9193a403fbb0d8bf004283717b6
alfaArghya <arghya.coding@gmail.com>
2024-04-01 15:17:52 +0530

    export all blog routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,4 +2,8 @@ export { default as signin } from "./signin";
 
 export { default as signup } from "./signup";
 
-export * from "./blog";
+export { default as createBlog } from "./blog/createBlog";
+export { default as updateBlog } from "./blog/updateBlog";
+export { default as readBlog } from "./blog/readBlog";
+export { default as readAllBlog } from "./blog/readAllBlog";
+export { default as deleteBlog } from "./blog/deleteBlog";
