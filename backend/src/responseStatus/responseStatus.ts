41d2bdbf5a8c2bbd926b583a04861d08172bd9e8
alfaArghya <arghya.coding@gmail.com>
2024-04-01 10:32:27 +0530

    response status codes

---
+++ backend/src/responseStatus/responseStatus.ts
@@ -0,0 +1,10 @@
+export enum status {
+  Success = 200,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  RequestTimeout = 408,
+  Conflict = 409,
+  InternalServerError = 500,
+  BadGateway = 502,
+}
