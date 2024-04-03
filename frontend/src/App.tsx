47605a9629130a3f67d645798963185854dedd95
alfaArghya <arghya.coding@gmail.com>
2024-04-03 16:49:05 +0530

    all routes for pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,21 @@ import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Blogs from "./pages/Blogs";
 import Blog from "./pages/Blog";
+import CreateBlog from "./pages/CreateBlog";
+import UpdateBlog from "./pages/UpdateBlog";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Signup />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/createBlog" element={<CreateBlog />} />
+          <Route path="/updateBlog" element={<UpdateBlog />} />
         </Routes>
       </BrowserRouter>
     </>
