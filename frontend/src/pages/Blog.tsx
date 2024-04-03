dc927696015a9e39477eaf5e15d8b9456e651650
alfaArghya <arghya.coding@gmail.com>
2024-04-03 16:46:13 +0530

    read a single blog post

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,5 +1,28 @@
+import { useParams } from "react-router-dom";
+import Appbar from "../components/Appbar";
+import { useBlog } from "../hooks";
+import BlogRead from "../components/BlogRead";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  const { id } = useParams();
+  const { loading, blog } = useBlog({ id: id || "" });
+
+  if (loading) {
+    return <div>Loading . . . . </div>;
+  }
+  return (
+    <div>
+      <Appbar />
+      <div className="flex justify-center flex-col items-center mt-10">
+        <BlogRead
+          id={blog.id}
+          username={blog.author.username}
+          title={blog.title}
+          content={blog.content}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
