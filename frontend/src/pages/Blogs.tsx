1b72ab307d0a222733a2891681131b058e928adc
alfaArghya <arghya.coding@gmail.com>
2024-04-03 16:45:51 +0530

    all blog posts page

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -1,5 +1,29 @@
-const Blogs = () => {
-  return <div>Blog</div>;
+import Appbar from "../components/Appbar";
+import BlogCard from "../components/BlogCard";
+import { useBlogs } from "../hooks";
+
+const Blog = () => {
+  const { loading, blogs } = useBlogs();
+
+  if (loading) {
+    return <div>Loading . . . . </div>;
+  }
+
+  return (
+    <div>
+      <Appbar />
+      <div className="flex justify-center flex-col items-center mt-10">
+        {blogs.map((blog) => (
+          <BlogCard
+            id={blog.id}
+            username={blog.author.username}
+            title={blog.title}
+            content={blog.content}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Blogs;
+export default Blog;
