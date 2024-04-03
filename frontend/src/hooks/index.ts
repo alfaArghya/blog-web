31804144d3e17741c05222d537080f0525686157
alfaArghya <arghya.coding@gmail.com>
2024-04-03 16:39:10 +0530

    custom hooks

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { API_URL } from "../config";
+import { BlogType } from "../types";
+
+export const useBlogs = () => {
+  const [loading, setLoading] = useState(true);
+  const [blogs, setBlogs] = useState<BlogType[]>([]);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/v1/blog/all`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        setBlogs(response.data.blogs);
+        setLoading(false);
+      })
+      .catch((err) => {});
+  }, []);
+
+  return {
+    loading,
+    blogs,
+  };
+};
+
+export const useBlog = ({ id }: { id: string }) => {
+  const [loading, setLoading] = useState(true);
+  const [blog, setBlog] = useState<BlogType>();
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/v1/blog/${id}`, {
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        setBlog(response.data.blog);
+        setLoading(false);
+      })
+      .catch((err) => {});
+  }, [id]);
+
+  return {
+    loading,
+    blog,
+  };
+};
